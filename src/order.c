32de21935aded81398c53f57971a4ce57073bc34
matteo <matteo.menichetti@stud.unifi.it>
2023-11-02 18:21:47 +0100

    aggiornamento

--- src/order.c
+++ src/order.c
@@ -3,12 +3,11 @@
 int order(int * array, int length){
     if(length<=0){
         printf("selectionSort: length<=0");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
     if(array==NULL) {
         perror("selectionSort: array is NULL");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
