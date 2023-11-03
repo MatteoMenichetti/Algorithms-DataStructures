38fe1701a3a7af9cc06b3163fbc7ae48bd6140b0
matteo <matteo.menichetti98@gmail.com>
2023-11-03 17:30:52 +0100

    terminata implementazione di insertionSort.c

--- src/insertionSort.c
+++ src/insertionSort.c
@@ -1,21 +1,29 @@
 #include "../lib/common.h"
 
-void swap(int *array, int i, int j) {
-
+void swap(int *array, int start, int limit) {
+    int swap = array[limit];
+    for (int i = limit; i > start; i--) {
+        array[i] = array[i-1];
+    }
+    array[start] = swap;
 }
 
 int sort(int *array, int length) {
     if (control(array, length) != EXIT_SUCCESS)
         return EXIT_FAILURE;
 
-    for(int i = 1; i<length; i++){
-        for(int j=i-1; j>0; j++){
-            if (array[j]<array[i]){
-                swap(array, i, j);
-            }
+    int min = -1;
+
+    for (int i = 1; i < length; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (array[i] < array[j])
+                min = j;
+        }
+        if (min >= 0) {
+            swap(array, min, i);
+            min = -1;
         }
     }
-
     return EXIT_SUCCESS;
 }
 
