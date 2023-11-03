3e451b88faa6bc1b5672fd2da65a1bd2d0114b5d
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 10:50:20 +0100

    aggiornamento al for migliorato

--- tests/SelectionSort.cpp
+++ tests/SelectionSort.cpp
@@ -4,13 +4,13 @@
 TEST(SelectionSort, sort){
     int array[10] = {9,8,6,5,7,4,3,1,2,0}, arraySorted[10] = {0,1,2,3,4,5,6,7,8,9}, length = sizeof(array)/ sizeof(array[0]);
     sort(array, length);
-    for (int i=0; i < length; i++)
+    for (int i : array)
         ASSERT_EQ(array[i],arraySorted[i]);
 }
 
 TEST(SelectionSort, sortElEqual){
     int array[5] = {0,0,0,0,0};
     sort(array, 5);
-    for (int i=0; i < 5; i++)
-        ASSERT_EQ(array[i],0);
+    for (int i : array)
+        ASSERT_EQ(i,0);
 }
