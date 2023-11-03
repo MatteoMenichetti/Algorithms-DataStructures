1b008283302e736d8986ed8ff7da9af7d534cbf4
matteo <matteo.menichetti98@gmail.com>
2023-11-03 17:31:32 +0100

    refactor dello scambio di elementi in swap

--- src/selectionSort.c
+++ src/selectionSort.c
@@ -5,17 +5,21 @@ int sort(int *array, int length) {
 
     if(o!=EXIT_SUCCESS)return o;
 
-    for (int i = 0; i < length - 2; i++) {
+    for (int i = 0; i < length - 1; i++) {
         min = i;
         for (int j = i + 1; j < length; j++) {
             if (array[j] < array[min])
                 min = j;
         }
         if (min != i) {
-            int t = array[min];
-            array[min] = array[i];
-            array[i] = t;
+            swap(array, min, i);
         }
     }
     return EXIT_SUCCESS;
+}
+
+void swap(int *array, int min, int i) {
+    int t = array[min];
+    array[min] = array[i];
+    array[i] = t;
 }
