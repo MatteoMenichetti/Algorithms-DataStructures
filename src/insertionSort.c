06c3dedf545caf20e8562f70f609c3a263113ad0
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 12:29:50 +0100

    work in progress

--- src/insertionSort.c
+++ src/insertionSort.c
@@ -1,8 +1,22 @@
 #include "../lib/common.h"
 
+void swap(int *array, int i, int j) {
+
+}
+
 int sort(int *array, int length) {
     if (control(array, length) != EXIT_SUCCESS)
         return EXIT_FAILURE;
 
+    for(int i = 1; i<length; i++){
+        for(int j=i-1; j>0; j++){
+            if (array[j]<array[i]){
+                swap(array, i, j);
+            }
+        }
+    }
+
     return EXIT_SUCCESS;
 }
+
+
