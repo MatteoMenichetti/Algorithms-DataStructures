a66f6caccf63b476ff211925fad6b15f90e587bf
matteo <matteo.menichetti98@gmail.com>
2023-11-03 17:34:58 +0100

    reformat ed aggiunto test

--- tests/SelectionSort.cpp
+++ tests/SelectionSort.cpp
@@ -12,21 +12,28 @@ TEST(InsertionSort, sort) {
 }
 
 TEST(InsertionSort, sort1) {
-    int array[4]={0,1,2,3};
+    int array[4] = {0, 1, 2, 3};
     sort(array, 4);
     for (int i = 0; i < 4; i++)
         EXPECT_EQ(array[i], i);
 }
 
 TEST(InsertionSort, sort2) {
-    int array[4]={3,2,1,0};
+    int array[4] = {3, 2, 1, 0};
     sort(array, 4);
     for (int i = 0; i < 4; i++)
         EXPECT_EQ(array[i], i);
 }
 
-TEST(SelectionSort, swap){
-    int array[2]={0, 1};
+TEST(InsertionSort, sort3) {
+    int array[4] = {3, 2, 1, 2}, arraySorted[4] = {1, 2, 2, 3};
+    sort(array, 4);
+    for (int i = 0; i < 4; i++)
+        EXPECT_EQ(array[i], arraySorted[i]);
+}
+
+TEST(SelectionSort, swap) {
+    int array[2] = {0, 1};
     swap(array, 0, 1);
     ASSERT_EQ(array[0], 1);
     ASSERT_EQ(array[1], 0);
