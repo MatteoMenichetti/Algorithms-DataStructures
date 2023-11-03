e7380ac7b617bef8afa26f558bd67b6f1d1a2dd5
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 12:04:34 +0100

    refactor order

--- tests/CommonTests.cpp
+++ tests/CommonTests.cpp
@@ -1,22 +1,22 @@
 #include <gtest/gtest.h>
-#include "../src/order.c"
+#include "../src/control.c"
 
 int natural[10] = {9, 8, 6, 5, 7, 4, 3, 1, 2, 0};
 TEST(CommonTest, CorrectSize) {
-    int o = order(natural, 10);
+    int o = control(natural, 10);
 
     EXPECT_EQ(o, EXIT_SUCCESS);
 }
 
 TEST(CommonTest, SizeMinorEqual2El) {
-    int o = order(natural, -1);
+    int o = control(natural, -1);
 
     EXPECT_EQ(o, EXIT_FAILURE);
 
-    o = order(natural, 1);
+    o = control(natural, 1);
     EXPECT_EQ(o, EXIT_FAILURE);
 }
 
 TEST(CommonTest, NULLArray) {
-    ASSERT_EQ(order(NULL, 1), EXIT_FAILURE);
+    ASSERT_EQ(control(NULL, 1), EXIT_FAILURE);
 }
