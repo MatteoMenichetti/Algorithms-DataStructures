1b9da5f234786e5bbe521b8056702cd3fa3cc86b
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 10:39:25 +0100

    implementato sort

--- src/selectionSort.c
+++ src/selectionSort.c
@@ -1,6 +1,26 @@
 #include "../lib/common.h"
 
 int selectionSort(int * array, int length){
+
     order(array, length);
-    return EXIT_FAILURE;
+
+    return sort(array, length);
+}
+
+int sort(int *array, int length){
+    int min;
+
+    for(int i = 0; i < length-2; i++){
+        min = i;
+        for(int j=i+1; j < length; j++){
+            if (array[j]<array[min])
+                min=j;
+        }
+        if(min != i){
+            int t = array[min];
+            array[min]=array[i];
+            array[i]=t;
+        }
+    }
+    return EXIT_SUCCESS;
 }
