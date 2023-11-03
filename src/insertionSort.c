492dfd9fd6b27e6f95e597e979539a4990a4397d
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 12:12:24 +0100

    reformat

--- src/insertionSort.c
+++ src/insertionSort.c
@@ -1,7 +1,7 @@
 #include "../lib/common.h"
 
-int sort(int *array, int length){
-    if(control(array, length)!=EXIT_SUCCESS)
+int sort(int *array, int length) {
+    if (control(array, length) != EXIT_SUCCESS)
         return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
