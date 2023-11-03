980b36573f2037c8888b32aa4a1cce0f28180fcf
matteo <matteo.menichetti98@gmail.com>
2023-11-03 17:31:08 +0100

    completati test InsertionSort.cpp

--- tests/InsertionSort.cpp
+++ tests/InsertionSort.cpp
@@ -4,11 +4,30 @@ extern "C" {
 #include "../lib/common.h"
 }
 
-
 TEST(InsertionSort, sort) {
-    int array[10] = {9, 8, 6, 5, 7, 4, 3, 1, 2, 0}, arraySorted[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, length =
-            sizeof(array) / sizeof(array[0]);
-    sort(array, length);
-    for (int i: array)
-        ASSERT_EQ(array[i], arraySorted[i]);
+    int array[4] = {1, 2, 3, 0};
+    sort(array, 4);
+    for (int i = 0; i < 4; i++)
+        EXPECT_EQ(array[i], i);
+}
+
+TEST(InsertionSort, sort1) {
+    int array[4]={0,1,2,3};
+    sort(array, 4);
+    for (int i = 0; i < 4; i++)
+        EXPECT_EQ(array[i], i);
+}
+
+TEST(InsertionSort, sort2) {
+    int array[4]={3,2,1,0};
+    sort(array, 4);
+    for (int i = 0; i < 4; i++)
+        EXPECT_EQ(array[i], i);
+}
+
+TEST(InsertionSort, swap) {
+    int array[3] = {1, 2, 0};
+    swap(array, 0, 2);
+    for (int i = 0; i < 3; i++)
+        ASSERT_EQ(array[i], i);
 }
