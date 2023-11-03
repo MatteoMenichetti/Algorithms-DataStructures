e9720df17dbdac4a95bfc21d68962426a48d21a2
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 12:05:22 +0100

    prima implementazione

--- tests/InsertionSort.cpp
+++ tests/InsertionSort.cpp
@@ -1,3 +1,10 @@
-//
-// Created by matteo on 03/11/23.
-//
+#include <gtest/gtest.h>
+#include "../src/insertionSort.c"
+
+TEST(InsertionSort, sort) {
+    int array[10] = {9, 8, 6, 5, 7, 4, 3, 1, 2, 0}, arraySorted[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, length =
+            sizeof(array) / sizeof(array[0]);
+    sort(array, length);
+    for (int i: array)
+        ASSERT_EQ(array[i], arraySorted[i]);
+}
