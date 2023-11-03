cad8228eab5b49ad0d5600b2a804744b78593271
matteo <matteo.menichetti@stud.unifi.it>
2023-11-03 10:47:22 +0100

    Aggiornato test per includere minore uguale a 2. natural fatta diventare globale

--- tests/CommonTests.cpp
+++ tests/CommonTests.cpp
@@ -1,20 +1,19 @@
 #include <gtest/gtest.h>
-
 #include "../src/order.c"
 
-TEST(CommonTest, SorrectSize){
-    int natural [10] = {9,8,6,5,7,4,3,1,2,0}, o = order(natural, 10);
+int natural [10] = {9,8,6,5,7,4,3,1,2,0};
+TEST(CommonTest, CorrectSize){
+    int o = order(natural, 10);
 
     EXPECT_EQ(o, EXIT_SUCCESS);
 }
 
-TEST(CommonTest, SizeMinorEqual0El){
-    int natural [10] = {9,8,6,5,7,4,3,1,2,0};
+TEST(CommonTest, SizeMinorEqual2El){
     int o = order(natural, -1);
 
     EXPECT_EQ(o, EXIT_FAILURE);
 
-    o = order(natural, 0);
+    o = order(natural, 1);
     EXPECT_EQ(o, EXIT_FAILURE);
 }
 
