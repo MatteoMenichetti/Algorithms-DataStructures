652158cc0311eda613a6d187672131a442dd23c2
matteo <matteo.menichetti@stud.unifi.it>
2023-11-04 20:43:30 +0100

    implementato correttamente quick_sort

--- src/quickSort.c
+++ src/quickSort.c
@@ -10,20 +10,27 @@ int sort(int *array, int length) {
 }
 
 void quick_sort(int *array, int l, int r) {
-    int i = partition(array, l, r);
-    quick_sort(array, i + 1, r);
-    quick_sort(array, l, i - 1);
+    if (l < r) {
+        int i = partition(array, l, r);
+        quick_sort(array, i + 1, r);
+        quick_sort(array, l, i - 1);
+    }
 }
 
 // 3, 2, 1, 0
 
 int partition(int *array, int l, int r) {
-    int i;
-
-    for (int j = r - 1; j >= 0; j--) {
-
+    int i = l, j = r - 1;
+    while (i < j) {
+        while (array[j] > array[r]) {
+            j--;
+        }
+        while (array[i] < array[r]) {
+            i++;
+        }
+        swap(array, i, j);
     }
-
+    swap(array, i, r);
     return i;
 }
 
