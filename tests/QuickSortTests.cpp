06acda9e3b3a999289ba203276557cc0e597cb0b
matteo <matteo.menichetti@stud.unifi.it>
2023-11-05 17:53:40 +0100

    cambiato ordine test

--- tests/QuickSortTests.cpp
+++ tests/QuickSortTests.cpp
@@ -4,6 +4,29 @@ extern "C" {
 #include "../lib/quick_sort.h"
 }
 
+TEST(QuickSort, partition) {
+    int array[4] = {1, 2, 3, 0}, mid_array[4] = {0, 2, 3, 1};
+    int i = partition(array, 0, 3);
+    ASSERT_EQ(i, 0);
+    for (int j = 0; j < 4; j++)
+        ASSERT_EQ(array[i], mid_array[i]);
+}
+
+TEST(QuickSort, partitionOrdElem) {
+    int array[4] = {0, 1, 2, 3}, mid_array[4] = {0, 1, 2, 3};
+    int i = partition(array, 0, 3);
+    ASSERT_EQ(i, 3);
+    for (int j = 0; j < 4; j++)
+        ASSERT_EQ(array[i], mid_array[i]);
+}
+
+TEST(QuickSort, swap) {
+    int array[3] = {2, 1, 0};
+    swap(array, 0, 2);
+    for (int i = 0; i < 3; i++)
+        ASSERT_EQ(array[i], i);
+}
+
 TEST(QuickSort, sort) {
     int array[4] = {1, 2, 3, 0};
     sort(array, 4);
@@ -37,19 +60,4 @@ TEST(QuickSort, sort4) {
     sort(array, 4);
     for (int i = 0; i < 4; i++)
         EXPECT_EQ(array[i], arraySorted[i]);
-}
-
-TEST(QuickSort, partition) {
-    int array[4] = {3, 2, 1, 0}, mid_array[4] = {0, 3, 2, 1};
-    int i = partition(array, 0, 3);
-    ASSERT_EQ(i, 0);
-    for (int j = 0; j < 4; j++)
-        ASSERT_EQ(array[i], mid_array[i]);
-}
-
-TEST(QuickSort, swap) {
-    int array[3] = {2, 1, 0};
-    swap(array, 0, 2);
-    for (int i = 0; i < 3; i++)
-        ASSERT_EQ(array[i], i);
 }
