b1d5606bbc85e7d03eab0d7581638bf0f3f13851
matteo <matteo@matteo.com>
2023-12-14 17:59:59 +0100

    first implementation of up_heap

--- src/dataStructure/heap.c
+++ src/dataStructure/heap.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 
-typedef struct heap{
+typedef struct heap {
     int *array;
     int n;
     int effective;
 } heap;
 
-heap * construct(int n){
-    if(n<=3)
+heap *construct(int n) {
+    if (n <= 3)
         return NULL;
 
-    heap* heap = malloc(sizeof(heap));
+    heap *heap = malloc(sizeof(heap));
 
     heap->array = calloc(n, sizeof(int));
     heap->n = n;
@@ -18,18 +18,28 @@ heap * construct(int n){
     return heap;
 }
 
-void up_heap(heap* heap){
+// [6, 4, 7, 3, 2, 1]
 
+void up_heap(int array[]) {
+    int j = sizeof(array)/ sizeof(int);
+    for(int i = j/2 ; j>0; j/=2){
+        if(array[j]>array[i]){
+            int t = array[j];
+            array[j]=array[i];
+            array[i]=t;
+        }
+    }
 }
 
-int insert(heap * heap, int n){
-    if(heap == NULL)return -1;
-    if(heap->effective==heap->n)
+int insert(heap *heap, int n) {
+    if (heap == NULL)
         return -1;
-    heap->array[heap->effective] = n;
+    if (heap->effective == heap->n)
+        return -1;
+    heap->array[heap->effective + 1] = n;
     heap->effective++;
 
-    up_heap(heap);
+    up_heap(heap->array);
 
     return 1;
 }
