d9af3ccce6878d6710a51d6a02e01013e742e46c
matteo <matteo@matteo.com>
2023-12-22 15:35:57 +0100

    update pointer

--- src/dataStructure/heap.c
+++ src/dataStructure/heap.c
@@ -10,18 +10,17 @@ heap *construct(int n) {
     if (n <= 3)
         return NULL;
 
-    heap *heap = malloc(sizeof(heap));
+    heap *head = (struct heap *) malloc(sizeof(heap));
 
-    heap->array = calloc(n, sizeof(int));
-    heap->n = n;
-    heap->effective = 0;
-    return heap;
+    head->array = (int *) calloc(n, sizeof(int));
+    head->n = n;
+    head->effective = 0;
+    return head;
 }
 
 // [6, 4, 7, 3, 2, 1]
 
-void up_heap(int array[]) {
-    int j = sizeof(array)/ sizeof(int);
+void up_heap(int array[], int j) {
     for(int i = j/2 ; j>0; j/=2){
         if(array[j]>array[i]){
             int t = array[j];
@@ -39,7 +38,7 @@ int insert(heap *heap, int n) {
     heap->array[heap->effective + 1] = n;
     heap->effective++;
 
-    up_heap(heap->array);
+    up_heap(heap->array, n);
 
     return 1;
 }
