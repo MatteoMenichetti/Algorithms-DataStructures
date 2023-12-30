00c60a3bcf02080df98127c0f1f53a84215f4934
matteo <matteo@matteo.com>
2023-12-30 12:27:52 +0100

    include test on construct

--- tests/dataStructure/HeapTest.cpp
+++ tests/dataStructure/HeapTest.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include "../../src/dataStructure/heap.c"
 
-TEST(HeapTest, insert){
-    heap * heap = construct(1);
+TEST(HeapTest, constructNminoreq3){
+    ASSERT_EQ((long)construct(2),NULL);
+}
+
+TEST(HeapTest, insert) {
+    heap *heap = construct(4);
 
     insert(heap, 2);
 
@@ -14,19 +18,18 @@ TEST(HeapTest, insert){
 //36 100 17 19
 
 TEST(HeapTest, insert1){
-    heap * heap = construct(4);
+    heap * heap = construct(3);
     int array[] = { 19, 36, 17, 100},sortedArray[] = {100, 36, 17, 19};
 
     for(int i : array)
         insert(heap, i);
 
     for (int i = 0; i<4; i++)
-        ASSERT_EQ(heap->array[i], array[i]);
+        ASSERT_EQ(heap->array[i], sortedArray[i]);
 }
 
 TEST(HeapTest, remove){
     heap * heap = construct(4);
     int array[] = { 100, 19, 36, 17}, sortedArray[] = {36, 19, 17};
-
 
 }
